7d0e041b3a5ab59efb6b06f8060a493dff9b4959
Nayso7 <108063965+Nayso7@users.noreply.github.com>
2024-06-03 22:01:30 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+    $Litre = 0;
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<img src="barille.png" alt="miam" >
+<h3>Buy some kerosene</h3>
+<h5>How much ?</h5>
+<form action="formulaire.php" method="POST">
+    <input type="text" id="Litre" name="Litre" value=""><br>
+    <input type="submit" value="Buy">
+</form>
+<h4>By Gaga</h4>
+</body>
+</html>
